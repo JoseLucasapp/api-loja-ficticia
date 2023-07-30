b8705b888b23f48780245ed172f1bb2f4b735b7e
José Lucas <josluc2016@gmail.com>
2023-07-30 01:57:11 -0300

    feat: home page

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ app.use(express.json())
 app.use(cors())
 app.use('/api', router)
 
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>')
+})
+
 productRoutes(router)
 
 app.listen(port, () => console.log(`running on port ${port}`))
