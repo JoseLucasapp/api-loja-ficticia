587a12af1ecc295a7d46f4cd6ec0fbf6a064e416
José Lucas <josluc2016@gmail.com>
2023-07-30 14:23:29 -0300

    feat: port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const productRoutes = require('./routes/products')
 require('./database/connection')
 
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 const router = express.Router()
 
 app.use(express.json())
